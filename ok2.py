344775a65dc76d9aba1a3b29b2ac0dc30b1d209a
Student <egorbelevcev@gmail.con>
2023-12-02 14:46:03 +0300

    kug

--- ok2.py
+++ ok2.py
@@ -1,2 +1,16 @@
-a=int(input())
-b=int(input())
+import time
+a=int(input("введите часы"))
+b=int(input("введите минуты"))
+c=int(input("введите секунды"))
+while True:
+    if b >= 60:
+        a+=1
+        b=0
+    if c >= 60:
+        b+=1
+        c=0
+    if a >= 24:
+        a=0
+    c+=1
+    print(a,":",b,":",c)
+    time.sleep(1)
